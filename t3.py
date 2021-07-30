03d8875441176f155807b28ca63f5f7ec58c6a52
soumya13042000 <soumyashreebaral086@gmail.com>
2021-07-30 11:22:44 +0800

    not required

--- t3.py
+++
@@ -1,3 +0,0 @@
-n1,n2,n3=int(input('Enter your first number')),int(input('Enter your second number')),int(input('Enter your third number'))
-smaller='first number is smaller'if n2>n1<n3 else'second number is smaller'if n1>n2<n3 else'third number is smaller'
-print(smaller)
